3fc1acf8e36f0e7706330b3fe764e0d8ea0e415d
unknown <105841109422@student.unismuh.ac.id>
2024-08-22 19:44:17 +0800

    selesai chapter 5 kiw

--- app/ui/dashboard/layout.js
+++ app/ui/dashboard/layout.js
@@ -1,23 +1,18 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
-
-export default function Layout({ children }) {
-  return (
-    <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
-      <div className="flex-none w-full md:w-64">
-        <SideNav />
-      </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
-  );
-}
 import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts';
-export default function RootLayout({
-  children,
-}) {
+import { lusitana } from '@/app/ui/fonts';
+
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>
+        <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
+          <div className="flex-none w-full md:w-64">
+            <SideNav />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
--- app/ui/dashboard/nav-links.js
+++ app/ui/dashboard/nav-links.js
@@ -1,31 +1,25 @@
-// import {
-//   UserGroupIcon,
-//   HomeIcon,
-//   DocumentDuplicateIcon,
-// } from '@heroicons/react/24/outline';
 "use client";
 
-import {
-  UserGroupIcon,
-  HomeIcon,
-  DocumentDuplicateIcon,
-} from "@heroicons/react/24/outline";
+import { UserGroupIcon, HomeIcon, InboxIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import clsx from "clsx";
 
-
-
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: "Home",
+    href: "/",
+    icon: HomeIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  {
+    name: "Users",
+    href: "/users",
+    icon: UserGroupIcon,
+  },
+  {
+    name: "Inbox",
+    href: "/inbox",
+    icon: InboxIcon,
+  },
 ];
 
 export default function NavLinks() {
@@ -34,17 +28,15 @@ export default function NavLinks() {
   return (
     <>
       {links.map((link) => {
+        const isActive = pathname === link.href;
         const LinkIcon = link.icon;
+        
         return (
           <Link
             key={link.name}
             href={link.href}
-            className={clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
-              {
-                "bg-sky-100 text-blue-600": pathname === link.href,
-              }
-            )}
+            className={`flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3
+              ${isActive ? 'bg-blue-100 text-blue-600' : 'bg-gray-50 hover:bg-sky-100 hover:text-blue-600'}`}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
